9353dadc044e0fb9bd5fd6bb31206f0921ed9602
Unknown <steffenkaminski@hotmail.de>
2018-10-09 10:58:51 +0200

    changed two producer example

--- Two_producer_example.jl
+++ Two_producer_example.jl
@@ -8,8 +8,12 @@ c = [1 1]
 
 @expression(m, profit1, (a-sum(q))*q[1] - c[1]*q[1])
 @expression(m, profit2, (a-sum(q))*q[2] - c[2]*q[2])
+
 @objective(m, :Max, profit1 + profit2)
 
 solve(m)
 @show getvalue(q)
+println("Price is $(a-sum(getvalue(q)))" )
+println("Profit 1 is $(getvalue(profit1))" )
+println("Profit 2 is $(getvalue(profit2))" )
 println("Price is $(a-sum(getvalue(q)))" )
