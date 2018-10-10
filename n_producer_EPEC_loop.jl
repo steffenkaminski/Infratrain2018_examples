50a4e9ff6134e1a4837d2f1d6d5dec4e18cac80c
Unknown <steffenkaminski@hotmail.de>
2018-10-10 11:28:31 +0200

    added EPEC example using loop calculation of MPECs

--- n_producer_EPEC_loop.jl
+++ n_producer_EPEC_loop.jl
@@ -1,13 +1,17 @@
 #steffen.kaminski@kuleuven.be
 #software used: Atom, Julia 0.6.4
 
-#example of slide 56-57 using loops for finding marked equilibrium
+#example of slide 79-82 using loops for finding marked equilibrium
+#for each iteration a MPEC is solved
 
 using JuMP
+using Complementarity
 using Ipopt
 
 abs_err = 0.0001
-num_producer = 3
+num_leader = 2
+num_follower = 1
+num_producer = num_leader + num_follower
 a = 10
 c = ones(num_producer) * 1
 
@@ -17,6 +21,8 @@ collection_q = []
 err = 10 #just a initialization
 i=1
 q_last =  ones(num_producer) * 0.0 #just a initialization value
+
+##
 while err > abs_err
     err = 0
     for producer in 1:num_producer
@@ -27,9 +33,8 @@ while err > abs_err
         if i != 1
             println( "i is $(i)")
             println(q_last)
-            for k in 1:num_producer
+            for k in 1:num_leader #just fix quantities for leaders
                 if k != producer
-                    #fix profits of other producers
                     @constraint(m, q[k] == q_last[k])
                 end
             end
@@ -37,7 +42,11 @@ while err > abs_err
         #@constraint(m, q2val, q[2] == 2.5)
 
         global profits = @NLexpression(m, profit[i in 1:num_producer], (a-sum(q[j] for j in 1:num_producer))*q[i] - c[i]*q[i])
-        @NLobjective(m, Max, profit[producer])
+        @NLobjective(m, Max, q[producer]*(a - (sum(q[i] for i in 1:num_producer)))-c[producer]*q[producer])
+        for j = num_leader+1:num_producer
+            @complements(m, 0 <= c[j] - a + sum(q[i] for i in 1:num_producer) + q[j], q[j] >= 0)
+            # c(i) - a + b*(sum(j,q(j))-q(i)) + b*Q_L + 2*b*q(i) #GAMS
+        end
         solve(m)
         #println("Profit is $(getvalue(profit))" )
 
